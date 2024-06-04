e8153de8caef39a85854aa75a2b08f6e3bac51e8
Fuzzkatt <zonghan2000@gmail.com>
2024-06-03 17:40:23 -0700

    add further debug

--- torch/_dynamo/utils.py
+++ torch/_dynamo/utils.py
@@ -1251,6 +1251,7 @@ def same(
     relax_numpy_equality=False,
     ignore_non_fp=False,
     log_error=log.error,
+    key='',
 ):
     """Check correctness to see if ref and res match"""
     if fp64_ref is None:
@@ -1309,6 +1310,7 @@ def same(
                     relax_numpy_equality=relax_numpy_equality,
                     ignore_non_fp=ignore_non_fp,
                     log_error=log_error,
+                    key=k,
                 )
             ):
                 log_error("Accuracy failed for key name %s", k)
@@ -1399,7 +1401,9 @@ def same(
                 # if not passes_test:
                 if True:
                     log_error(
-                        "RMSE (res-fp64): %.5f, (ref-fp64): %.5f and shape=%s. res.dtype: %s, multiplier: %f, tol: %f",
+                        "key: %s, passes_test: %s, RMSE (res-fp64): %.5f, (ref-fp64): %.5f and shape=%s. res.dtype: %s, multiplier: %f, tol: %f",
+                        key,
+                        passes_test,
                         res_error,
                         ref_error,
                         res.size(),
@@ -1408,7 +1412,8 @@ def same(
                         tol,
                     )
                     # import pdb; pdb.set_trace()
-                return passes_test
+                return True
+                # return passes_test
 
             if ignore_non_fp:
                 return True
